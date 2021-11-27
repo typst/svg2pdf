a5886170c7d7a80e70d2f4b1331ae566d5281368
Martin Haug <mhaug@live.de>
2021-11-27 21:12:18 +0100

    Add capability for transforms

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use pdf_writer::{Content, Filter, Finish, Name, PdfWriter, Rect, Ref, TextStr};
 use std::collections::HashMap;
 use usvg::{
     Align, AspectRatio, FillRule, ImageKind, LineCap, LineJoin, Node, NodeExt, NodeKind,
-    Paint, PathSegment, Stop, Tree, ViewBox, Visibility,
+    Paint, PathSegment, Stop, Transform, Tree, ViewBox, Visibility,
 };
 
 pub struct Options {
@@ -126,7 +126,6 @@ impl CoordToPdf {
 
     /// Convert from x PDF coordinates to SVG source coordinates.
     fn svg_x(&self, x: f32) -> f64 {
-        println!("factor {}, offset {}", self.factor_x, self.offset_x);
         (self.pt_to_px(x) - self.offset_x) / self.factor_x
     }
 
@@ -303,14 +302,7 @@ pub fn from_tree(tree: &Tree, opt: Options) -> Option<Vec<u8>> {
     writer.catalog(catalog_id).pages(page_tree_id);
     writer.pages(page_tree_id).kids([page_id]);
 
-    dbg!(viewport);
-
-    let bbox = dbg!(Rect::new(
-        0.0,
-        0.0,
-        c.px_to_pt(viewport.0),
-        c.px_to_pt(viewport.1)
-    ));
+    let bbox = Rect::new(0.0, 0.0, c.px_to_pt(viewport.0), c.px_to_pt(viewport.1));
 
     let mut ctx = Context::new(&tree, &bbox, &c, opt.dpi, &mut next_id);
 
@@ -564,7 +556,7 @@ impl Render for usvg::Path {
             }
         }
 
-        draw_path(&self.data.0, content, ctx.c);
+        draw_path(&self.data.0, self.transform, content, ctx.c);
 
         match (self.fill.as_ref().map(|f| f.rule), self.stroke.is_some()) {
             (Some(FillRule::NonZero), true) => content.fill_and_stroke_nonzero(),
@@ -717,14 +709,6 @@ impl Render for usvg::Image {
 
                     let color = decoded.color();
 
-                    println!(
-                        "viewbox {:?}, rwidth in pt {}, width {:?}, height {:?}",
-                        self.view_box,
-                        ctx.c.px_to_pt(rect.width()),
-                        decoded.width(),
-                        decoded.height()
-                    );
-
                     let image_bytes: Vec<u8> =
                         if (color.bits_per_pixel() / color.channel_count() as u16) > 8 {
                             decoded
@@ -840,14 +824,24 @@ impl Render for usvg::Image {
 
                 let mut xobject = writer.form_xobject(external_ref, &content);
                 xobject.resources().x_objects().pair(xobj_name, image_ref);
-                xobject.bbox(dbg!(Rect::new(
+                xobject.bbox(Rect::new(
                     0.0,
                     0.0,
                     rect.width() as f32,
                     rect.height() as f32,
-                )));
-                let scaling = (72.0 / ctx.dpi) as f32;
-                xobject.matrix([scaling, 0.0, 0.0, scaling, 0.0, 0.0]);
+                ));
+
+                let scaling = 72.0 / ctx.dpi;
+                let mut transform = self.transform.clone();
+                transform.scale(scaling, scaling);
+                xobject.matrix([
+                    transform.a as f32,
+                    transform.b as f32,
+                    transform.c as f32,
+                    transform.d as f32,
+                    transform.e as f32,
+                    transform.f as f32,
+                ]);
 
                 external_ref
             } else {
@@ -872,7 +866,7 @@ fn apply_clip_path(path_id: Option<&String>, content: &mut Content, ctx: &mut Co
             for child in clip_path.children() {
                 match *child.borrow() {
                     NodeKind::Path(ref path) => {
-                        draw_path(&path.data.0, content, ctx.c);
+                        draw_path(&path.data.0, path.transform, content, ctx.c);
                         content.clip_nonzero();
                         content.end_path();
                     }
@@ -899,6 +893,7 @@ fn apply_mask(
             let (bbox, matrix) = if mask.content_units == usvg::Units::UserSpaceOnUse {
                 (*ctx.bbox, None)
             } else {
+                let (x, y) = mask_node.transform().apply(mask.rect.x(), mask.rect.y());
                 (
                     pdf_bbox,
                     Some([
@@ -906,8 +901,8 @@ fn apply_mask(
                         0.0,
                         0.0,
                         1.0,
-                        bbox.x() as f32 + ctx.c.x(mask.rect.x()),
-                        bbox.y() as f32 + ctx.c.y(mask.rect.y()),
+                        bbox.x() as f32 + ctx.c.x(x),
+                        bbox.y() as f32 + ctx.c.y(y),
                     ]),
                 )
             };
@@ -929,16 +924,27 @@ fn apply_mask(
     }
 }
 
-fn draw_path(path_data: &[PathSegment], content: &mut Content, c: &CoordToPdf) {
+fn draw_path(
+    path_data: &[PathSegment],
+    transform: Transform,
+    content: &mut Content,
+    c: &CoordToPdf,
+) {
     for &operation in path_data {
         match operation {
             PathSegment::MoveTo { x, y } => {
+                let (x, y) = transform.apply(x, y);
                 content.move_to(c.x(x), c.y(y));
             }
             PathSegment::LineTo { x, y } => {
+                let (x, y) = transform.apply(x, y);
                 content.line_to(c.x(x), c.y(y));
             }
             PathSegment::CurveTo { x1, y1, x2, y2, x, y } => {
+                let (x1, y1) = transform.apply(x1, y1);
+                let (x2, y2) = transform.apply(x2, y2);
+                let (x, y) = transform.apply(x, y);
+
                 content.cubic_to(c.x(x1), c.y(y1), c.x(x2), c.y(y2), c.x(x), c.y(y));
             }
             PathSegment::ClosePath => {
@@ -1104,60 +1110,27 @@ fn write_xobjects(pending_xobjects: &[(u32, Ref)], resources: &mut Resources) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
-    fn it_works() {
-        let doc = r##"<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path fill="#AF7E57" d="M15.898 34.875h-1.365s-1.27-3.348-1.074-5.736l2.801.549c.526 1.571-.362 5.187-.362 5.187zm4.144 0h1.365s1.27-3.348 1.074-5.736l-2.801.549c-.526 1.571.362 5.187.362 5.187z"/><path fill="#FA743E" d="M13.792 35.149c.102-.243.318-.694.341-.955.023-.261.034-.354.24-.426.347-.121 1.436-.087 1.623-.016.186.071.189.263.216.424s.114.424.227.64c.114.216.409.697.261 1.034-.062.14-.405.138-1.507.138s-1.417-.026-1.492-.118-.011-.477.091-.721zm8.356 0c-.102-.243-.318-.694-.341-.955-.023-.261-.034-.354-.24-.426-.347-.121-1.436-.087-1.623-.016-.187.071-.189.263-.216.424s-.114.424-.227.64-.409.697-.261 1.034c.062.14.405.138 1.507.138 1.102 0 1.417-.026 1.492-.118s.011-.477-.091-.721z"/><path fill="#AF7E57" d="M8.646 4.146c.011.127.871 1.535 1.323 2.635.452 1.1.71 2.198.948 2.448.476.499 2.092 1.879 3.117 3.343s.724 2.503.06 2.922c-.88.555-1.803.204-2.437-.788-.451-.706-1.951-3.209-2.739-4.601-.306-.541-.796-1.512-1.218-2.574-.397-1-.444-2.168-.504-2.312-.174-.425-1.12-.977-1.154-1.407 0 0 2.567-.095 2.604.334zm18.708 0c-.011.127-.871 1.535-1.323 2.635-.452 1.1-.71 2.198-.948 2.448-.476.499-2.092 1.879-3.117 3.343s-.724 2.503-.06 2.922c.88.555 1.803.204 2.437-.788.451-.706 1.951-3.209 2.739-4.601.304-.54.795-1.511 1.217-2.573.397-1 .444-2.168.504-2.312.175-.425 1.121-.977 1.155-1.406 0-.002-2.567-.097-2.604.332z"/><path fill="#99AAB5" d="M36 3.5c0 .276-.224.5-.5.5H.5C.224 4 0 3.776 0 3.5S.224 3 .5 3h35c.276 0 .5.224.5.5z"/><path fill="#292F33" d="M34 0c-.553 0-1 .448-1 1 0-.552-.447-1-1-1-.553 0-1 .448-1 1v5c0 .552.447 1 1 1 .553 0 1-.448 1-1 0 .552.447 1 1 1 .553 0 1-.448 1-1V1c0-.552-.447-1-1-1zM4 0c-.553 0-1 .448-1 1 0-.552-.447-1-1-1S1 .448 1 1v5c0 .552.447 1 1 1s1-.448 1-1c0 .552.447 1 1 1s1-.448 1-1V1c0-.552-.447-1-1-1z"/><path fill="#FA743E" d="M22.172 12.286c-.742.419-1.283 1.257-1.492 1.617-.665.117-1.151.3-1.151.3H16.47s-.486-.183-1.151-.3c-.209-.36-.75-1.197-1.492-1.617 0 0-2.121.937-2.746 1.521.21.537.492 1.047.716 1.417.4 1.092 1.502 2.527 1.626 2.777.389.78.741 4.5.741 4.5L18 24l3.835-1.5s.352-3.72.741-4.5c.125-.25 1.227-1.685 1.626-2.777.224-.37.506-.879.716-1.417-.625-.583-2.746-1.52-2.746-1.52z"/><g fill="#AF7E57"><path d="M27.096 4.363c-.33-.157-.476-.428-.377-.719l.003-.01c.099-.291.291-.346.738-.339 0 0 1.521.076 2.437.712 0 0-.456.304-.699.56v.01c-.185.541-1.894-.115-2.102-.214z"/><path d="M27.843 3.47c-.137-.033-.221-.173-.188-.31l.087-.357c.033-.137.173-.221.31-.188s.221.173.188.31l-.087.357c-.034.137-.173.221-.31.188zm-.445-.87c-.163-.055-.342.034-.397.197l-.279.837c-.055.164.168-.068.332-.013.163.055.342-.033.397-.196l.144-.427c.055-.164-.034-.342-.197-.398z"/><path d="M28.384 3.399c-.137-.033-.221-.173-.188-.31l.082-.334c.033-.137.173-.221.31-.188s.221.173.188.31l-.082.334c-.034.137-.173.221-.31.188z"/><path d="M28.9 3.424c-.137-.033-.221-.173-.188-.31l.068-.277c.033-.137.173-.221.31-.188s.221.173.188.31l-.068.277c-.034.137-.173.221-.31.188z"/><path d="M29.419 3.447c-.137-.033-.221-.173-.188-.31l.04-.162c.033-.137.173-.221.31-.188s.221.173.188.31l-.04.162c-.034.137-.174.222-.31.188z"/></g><g fill="#AF7E57"><path d="M8.904 4.363c.33-.157.476-.428.377-.719l-.003-.01c-.099-.29-.291-.346-.738-.339 0 0-1.521.076-2.437.712 0 0 .454.314.702.57.098.29 1.362.137 2.099-.214z"/><path d="M8.157 3.47c.137-.033.221-.173.188-.31l-.087-.357c-.033-.137-.173-.221-.31-.188-.137.034-.221.173-.188.31l.087.357c.034.137.173.221.31.188zm.445-.87c.163-.055.342.034.397.197l.279.837c.055.164-.168-.068-.332-.013-.163.055-.342-.033-.397-.196l-.144-.427c-.055-.164.034-.342.197-.398z"/><path d="M7.616 3.399c.137-.033.221-.173.188-.31l-.082-.334c-.033-.137-.173-.221-.31-.188-.136.034-.221.173-.187.31l.082.334c.033.137.172.221.309.188z"/><path d="M7.1 3.424c.137-.033.221-.173.188-.31l-.068-.277c-.033-.137-.172-.221-.309-.188s-.221.173-.188.31l.068.277c.033.137.172.221.309.188z"/><path d="M6.581 3.447c.137-.033.221-.173.188-.31l-.039-.162c-.034-.136-.173-.221-.31-.188-.137.034-.221.173-.188.31l.04.162c.033.137.173.222.309.188z"/></g><path fill="#DD551F" d="M21.84 24.5L18 23l-3.84 1.5s-.82 1.917-.96 5.188h3.06c.88-.979 1.2-3.604 1.2-3.604h1.08s.32 2.625 1.2 3.604h3.06c-.14-3.271-.96-5.188-.96-5.188z"/><path fill="#C1694F" d="M14.136 22.5h7.729v2h-7.729z"/><path fill="#14171A" d="M19 22.399h.65v2.209H19z"/><circle fill="#14171A" cx="18.5" cy="23.458" r=".167"/><path fill="#14171A" d="M18.021 23.458c0 .092-.075.167-.167.167-.092 0-.167-.075-.167-.167 0-.092.075-.167.167-.167.092.001.167.075.167.167z"/><circle fill="#14171A" cx="17.208" cy="23.458" r=".167"/><circle fill="#14171A" cx="16.562" cy="23.458" r=".167"/><circle fill="#14171A" cx="15.917" cy="23.458" r=".167"/><circle fill="#14171A" cx="15.271" cy="23.458" r=".167"/><circle fill="#14171A" cx="14.625" cy="23.458" r=".167"/><circle fill="#14171A" cx="17.208" cy="23.458" r=".167"/><circle fill="#657786" cx="16.553" cy="23.458" r=".148"/><path fill="#657786" d="M16.318 23.31h.235v.297h-.235z"/><path fill="#657786" d="M16.729 24.846h-.758v-2.697h.758v.35h-.409v1.997h.409z"/><path fill="#60352A" d="M19.591 3.84c-.823-.266-2.758-.194-3.242.436-1.258.024-2.734 1.161-2.928 2.661-.192 1.485.235 2.174.387 3.291.172 1.265.883 1.669 1.452 1.839.818 1.081 1.688 1.034 3.147 1.034 2.851 0 4.209-1.908 4.329-5.148.073-1.96-1.077-3.444-3.145-4.113z"/><path fill="#AF7E57" d="M16.778 12.286v2.444c0 .675.547 1.222 1.222 1.222.675 0 1.222-.547 1.222-1.222v-2.444h-2.444z"/><path fill="#AF7E57" d="M21.221 8.091c-.276-.382-.629-.69-1.403-.798.29.133.569.593.605.847.036.254.073.46-.157.206-.921-1.018-1.924-.617-2.917-1.239-.694-.434-.905-.915-.905-.915s-.085.641-1.137 1.294c-.305.189-.669.611-.871 1.234-.145.448-.1.847-.1 1.529 0 1.991 1.641 3.666 3.666 3.666s3.666-1.689 3.666-3.666c-.002-1.238-.132-1.722-.447-2.158z"/><path fill="#915A34" d="M19.629 12.286h-3.258s.407.814 1.629.814 1.629-.814 1.629-.814zm-1.222-1.019h-.815c-.113 0-.204-.091-.204-.204s.091-.204.204-.204h.815c.113 0 .204.091.204.204s-.091.204-.204.204z"/><path fill="#662113" d="M16.371 9.842c-.225 0-.407-.182-.407-.407v-.408c0-.225.182-.407.407-.407.225 0 .407.182.407.407v.407c0 .226-.182.408-.407.408zm3.258 0c-.225 0-.407-.182-.407-.407v-.408c0-.225.182-.407.407-.407.225 0 .407.182.407.407v.407c0 .226-.182.408-.407.408z"/></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/hello.pdf", buf).unwrap();
-    }
+    fn files() {
+        let paths = fs::read_dir("tests").unwrap();
+        for path in paths {
+            let path = path.unwrap();
+            let base_name = path.file_name().to_string_lossy().to_string();
+            if !base_name.ends_with(".svg") {
+                continue;
+            }
 
-    #[test]
-    fn test_gradient() {
-        let doc = r##"<svg width="120" height="240" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="a" x1="0" x2="1" y1="0" y2="1"><stop offset="0%" stop-color="red"/><stop offset="50%" stop-opacity="0"/><stop offset="100%" stop-color="#00f"/></linearGradient></defs><rect x="10" y="120" rx="15" ry="15" width="100" height="100" fill="url(#a)"/></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/gradient.pdf", buf).unwrap();
-    }
+            println!("{}", base_name);
 
-    #[test]
-    fn test_radial_gradient() {
-        let doc = r##"<svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><defs><radialGradient id="a"><stop offset="10%" stop-color="gold"/><stop offset="95%" stop-color="red"/></radialGradient></defs><circle cx="5" cy="5" r="4" fill="url(#a)"/></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/radial-gradient.pdf", buf).unwrap();
-    }
+            let doc = fs::read_to_string(path.path()).unwrap();
+            let buf = convert(&doc, Options::default()).unwrap();
 
-    #[test]
-    fn test_group() {
-        let doc = r##"<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><g opacity=".5"><circle fill="#00f" r="40"/><path fill="red" d="M0 0h80v60H0z"/></g></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/group.pdf", buf).unwrap();
-    }
+            let len = base_name.len();
+            let file_name = format!("{}.pdf", &base_name[0 .. len - 4]);
 
-    #[test]
-    fn test_clip_path() {
-        let doc = r##"<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><clipPath id="myClip"><circle cx="40" cy="35" r="35"/></clipPath><path id="heart" d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z" clip-path="url(#myClip)"/></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/clip-path.pdf", buf).unwrap();
-    }
-
-    #[test]
-    fn test_mask() {
-        let doc = r##"<svg viewBox="-10 -10 120 120" xmlns="http://www.w3.org/2000/svg"><mask id="a"><path fill="#fff" d="M0 0h100v100H0z"/><path d="M10 35a20 20 0 0 1 40 0 20 20 0 0 1 40 0q0 30-40 60-40-30-40-60Z"/></mask><path fill="orange" d="M-10 110h120V-10z"/><circle cx="50" cy="50" r="50" mask="url(#a)"/></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/mask.pdf", buf).unwrap();
-    }
-
-    #[test]
-    fn test_image() {
-        let doc = r##"<svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" xmlns:xlink="http://www.w3.org/1999/xlink"><image width="5" height="5" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="/></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/image.pdf", buf).unwrap();
-    }
-
-    #[test]
-    fn test_image_2() {
-        let doc = r##"<svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 5 5" xmlns:xlink="http://www.w3.org/1999/xlink"><image width="5" height="5" preserveAspectRatio="xMaxYMid slice" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAFCAIAAAAG+GGPAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAeUlEQVQImQFuAJH/AJLg//3+/8rryv9BNu3t0f///3/b/wD/hRz9pX7h4QD+/v7i4gz09KTpjkcA8z81MSYf+vDpf9v//f3wHx8c/0E2AP+GHeXeTOXgHf/9/eHhAPmziP+FGwB/2//8/O32yIT/Qjf8vqH//v6C2Psy2Ep/9lvaZwAAAABJRU5ErkJggg=="/></svg>"##;
-        let buf = convert(doc, Options::default()).unwrap();
-        std::fs::write("target/image-2.pdf", buf).unwrap();
+            std::fs::write(format!("target/{}", file_name), buf).unwrap();
+        }
     }
 }
