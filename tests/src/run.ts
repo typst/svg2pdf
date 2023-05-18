16365f213cdf87dfcbb4ab085103b8e63f45c418
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2023-05-18 23:08:06 +0200

    Update framework to ts

--- tests/src/run.ts
+++ tests/src/run.ts
@@ -1,32 +1,32 @@
-const pdf2img = require('pdf-img-convert');
-const fs = require("fs");
-const util = require('util');
-const path = require('path');
-const {
-    glob,
-    globSync,
-    globStream,
-    globStreamSync,
-    Glob,
-} = require('glob');
+import {glob} from 'glob';
+import path from "path";
+import {existsSync, mkdirSync} from "fs";
+import {promisify} from "util";
 
-const exec = util.promisify(require('child_process').exec);
+const exec = promisify(require('child_process').exec);
 const svgFilesPath = path.parse("files");
 const pdfFilesPath = path.parse("pdfs");
 const pdf2svgBinaryPath = path.join("..", "target", "release", "svg2pdf");
 
-async function buildPDF2SVG() {
+const SKIPPED_FILES = [
+    'structure/svg/zero-size.svg',
+    'structure/svg/not-UTF-8-encoding.svg',
+    'structure/svg/negative-size.svg',
+]
+
+async function buildBinary(): Promise<void> {
     try {
         console.log("Building pdf2svg...");
         await exec("cargo build --release --features cli");
         console.log("pdf2svg was build successfully.")
-    }   catch (e) {
+    } catch (e) {
         throw new Error("Build of pdf2svg failed.")
     }
 }
 
 async function buildPDFs() {
-    const svgFiles = await glob('**/*.svg', {cwd: svgFilesPath.name});
+    let svgFiles = await glob('**/*.svg', {cwd: svgFilesPath.name});
+    svgFiles = svgFiles.filter(el => !SKIPPED_FILES.includes(el));
 
     for (let filename of svgFiles) {
         let input = path.join(svgFilesPath.name, filename);
@@ -34,10 +34,8 @@ async function buildPDFs() {
         let output = path.join(outputFolder, path.parse(path.basename(filename)).name + ".pdf");
         let command = pdf2svgBinaryPath + ' ' + input + ' ' + output;
 
-        console.log(outputFolder)
-
-        if (!fs.existsSync(outputFolder)){
-            fs.mkdirSync(outputFolder, { recursive: true });
+        if (!existsSync(outputFolder)) {
+            mkdirSync(outputFolder, {recursive: true});
         }
 
         await exec(command);
@@ -48,9 +46,9 @@ async function buildPDFs() {
 (async function () {
 
     try {
-        await buildPDF2SVG();
+        await buildBinary();
         await buildPDFs();
-    }   catch (e) {
+    } catch (e: any) {
         console.error("Testing was unsuccessful. Error: " + e.message)
     }
 
