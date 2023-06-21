ca87794817d8e95209d455ab2096a1f4fc1821e2
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2023-06-21 13:23:10 +0200

    Improved handling of object bounding box

--- src/write/clip.rs
+++ src/write/clip.rs
@@ -1,8 +1,8 @@
-use crate::util::{calc_node_bbox_to_rect, Context, Units};
+use crate::util::{calc_node_bbox_to_rect, Context};
 use crate::write::group;
 use pdf_writer::{PdfWriter};
 use std::rc::Rc;
-use usvg::{Node, NodeKind, Transform};
+use usvg::{Node, NodeKind, Transform, Units};
 
 
 pub fn create_clip_path(
@@ -13,12 +13,6 @@ pub fn create_clip_path(
 ) -> String {
     ctx.context_frame.push();
     ctx.context_frame.append_transform(&clip_path.transform);
-
-    let parent_bbox = calc_node_bbox_to_rect(&parent, Transform::default());
-    match clip_path.units {
-        usvg::Units::ObjectBoundingBox => ctx.context_frame.set_units(Units::ObjectBoundingBox(Transform::from_bbox(parent_bbox))),
-        usvg::Units::UserSpaceOnUse => ctx.context_frame.set_units(Units::UserSpaceOnUse)
-    }
 
     let name = match *(*clip_path).root.borrow() {
         NodeKind::Group(ref group) => {
@@ -35,9 +29,18 @@ pub fn create_clip_path(
                 let name = ctx.alloc_soft_mask(group_ref);
                 name
             } else {
+                let parent_bbox = calc_node_bbox_to_rect(&parent, Transform::default());
+                ctx.context_frame.push();
+
+                if clip_path.units == Units::ObjectBoundingBox {
+                    ctx.context_frame.append_transform(&Transform::from_bbox(parent_bbox));
+                }
                 let (_, group_ref) =
                     group::create_x_object(group, &(*clip_path).root, writer, ctx);
                 let name = ctx.alloc_soft_mask(group_ref);
+
+                ctx.context_frame.pop();
+
                 name
             }
         }
