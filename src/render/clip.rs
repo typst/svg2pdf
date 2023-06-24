8aa5adea75fb4f2eda37b0e1ca64a4eff3391814
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2023-06-24 17:41:57 +0200

    Refactored clip path a bit

--- src/render/clip.rs
+++ src/render/clip.rs
@@ -13,41 +13,35 @@ pub(crate) fn render(
     ctx.context_frame.push();
     ctx.context_frame.append_transform(&clip_path.transform);
 
+    // TODO: Think about a more elegant way of solving this
+    match *clip_path.root.borrow_mut() {
+        NodeKind::Group(ref mut group) => {
+            if let Some(recursive_clip_path) = &clip_path.clip_path {
+                group.clip_path = Some(recursive_clip_path.clone());
+            }
+        }
+        _ => unreachable!(),
+    };
+
     let name = match *clip_path.root.borrow() {
         NodeKind::Group(ref group) => {
-            // TODO: Find a better way to do this
-            if let Some(recursive_clip_path) = &clip_path.clip_path {
-                let new_group = usvg::Group {
-                    clip_path: Some(recursive_clip_path.clone()),
-                    ..Default::default()
-                };
+            let parent_bbox = calc_node_bbox(parent, Transform::default())
+                .unwrap()
+                .to_rect()
+                .unwrap();
+            ctx.context_frame.push();
 
-                let new_node = Node::new(NodeKind::Group(new_group.clone()));
-                new_node.append(clip_path.root.make_deep_copy());
-
-                let (_, group_ref) =
-                    group::create_x_object(&new_node, &new_group, writer, ctx);
-
-                ctx.alloc_soft_mask(group_ref)
-            } else {
-                let parent_bbox = calc_node_bbox(parent, Transform::default())
-                    .unwrap()
-                    .to_rect()
-                    .unwrap();
-                ctx.context_frame.push();
-
-                if clip_path.units == Units::ObjectBoundingBox {
-                    ctx.context_frame
-                        .append_transform(&Transform::from_bbox(parent_bbox));
-                }
-                let (_, group_ref) =
-                    group::create_x_object(&clip_path.root, group, writer, ctx);
-                let name = ctx.alloc_soft_mask(group_ref);
-
-                ctx.context_frame.pop();
-
-                name
+            if clip_path.units == Units::ObjectBoundingBox {
+                ctx.context_frame
+                    .append_transform(&Transform::from_bbox(parent_bbox));
             }
+            let (_, group_ref) =
+                group::create_x_object(&clip_path.root, group, writer, ctx);
+            let name = ctx.alloc_soft_mask(group_ref);
+
+            ctx.context_frame.pop();
+
+            name
         }
         _ => unreachable!(),
     };
