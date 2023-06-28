66c5092275c766e79a0f5ddaeb336fad915ce8d7
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2023-06-28 23:26:18 +0200

    Code cleanup

--- src/render/image.rs
+++ src/render/image.rs
@@ -4,6 +4,7 @@ use crate::util::helper::{image_rect, NameExt, TransformExt};
 
 use pdf_writer::{Content, Filter, Finish, PdfWriter};
 use std::io::Cursor;
+use image::{ImageFormat, ImageOutputFormat};
 
 use usvg::{ImageKind, Node, Size, Transform, Tree, Visibility};
 
@@ -18,23 +19,10 @@ pub(crate) fn render(
         return;
     }
 
-    let (dynamic_image, image_data) = match &image.kind {
+    let (image_size, image_buffer) = match &image.kind {
         ImageKind::JPEG(content) => {
-            // We flip the image vertically because when applying the PDF base transformation the y axis will be flipped,
-            // so we need to undo that
-            let image = image::load_from_memory_with_format(
-                content.as_slice(),
-                image::ImageFormat::Jpeg,
-            )
-            .unwrap()
-            .flipv();
-            let mut buffer: Vec<u8> = Vec::new();
-            let mut writer = Cursor::new(&mut buffer);
-            image
-                .write_to(&mut writer, image::ImageOutputFormat::Jpeg(90))
-                .unwrap();
-            (image, buffer)
-        }
+            prepare_image(content.as_slice(), ImageFormat::Jpeg, ImageOutputFormat::Jpeg(100))
+        },
         ImageKind::SVG(tree) => {
             render_svg(image, tree, writer, content, ctx);
             return;
@@ -46,10 +34,10 @@ pub(crate) fn render(
 
     let (image_name, image_id) = ctx.deferrer.add_x_object();
 
-    let mut image_x_object = writer.image_xobject(image_id, &image_data);
+    let mut image_x_object = writer.image_xobject(image_id, &image_buffer);
     image_x_object.filter(Filter::DctDecode);
-    image_x_object.width(dynamic_image.width() as i32);
-    image_x_object.height(dynamic_image.height() as i32);
+    image_x_object.width(image_size.width() as i32);
+    image_x_object.height(image_size.height() as i32);
     image_x_object.color_space().device_rgb();
     image_x_object.bits_per_component(8);
     image_x_object.finish();
@@ -57,7 +45,7 @@ pub(crate) fn render(
     ctx.context_frame.append_transform(&image.transform);
     let image_rect = image_rect(
         &image.view_box,
-        Size::new(dynamic_image.width() as f64, dynamic_image.height() as f64).unwrap(),
+        image_size,
     );
 
     ctx.context_frame
@@ -72,12 +60,29 @@ pub(crate) fn render(
     ctx.context_frame.pop();
 }
 
+fn prepare_image(content: &[u8], input_format: ImageFormat, output_format: ImageOutputFormat) -> (Size, Vec<u8>) {
+    // We flip the image vertically because when applying the PDF base transformation the y axis will be flipped,
+    // so we need to undo that
+    let image = image::load_from_memory_with_format(
+        content,
+        input_format,
+    ).unwrap().flipv();
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut writer = Cursor::new(&mut buffer);
+    image
+        .write_to(&mut writer, output_format)
+        .unwrap();
+
+    let image_size = Size::new(image.width() as f64, image.height() as f64).unwrap();
+    (image_size, buffer)
+}
+
 fn render_svg(
     image: &usvg::Image,
     tree: &Tree,
     writer: &mut PdfWriter,
     content: &mut Content,
-    ctx: &mut Context,
+    ctx: &mut Context
 ) {
     ctx.context_frame.push();
     ctx.context_frame.append_transform(&image.transform);
