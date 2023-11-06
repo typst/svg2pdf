41758ea261bdf7710dd59e082f5aa39b0014a48b
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2023-11-06 14:51:26 -0300

    Put completions and man page in artifacts folder

--- crates/svg2pdf-cli/build.rs
+++ crates/svg2pdf-cli/build.rs
@@ -13,17 +13,19 @@ fn main() -> Result<(), std::io::Error> {
         Some(outdir) => outdir,
     };
 
-    // Put the files in the same level as the binary (e.g. /target/debug folder)
+    // Put the files in the artifacts folder (/target/artifacts)
     let outdir_path = &Path::new(&outdir_str).ancestors().nth(3).unwrap();
+    let artifacts_path = outdir_path.join("artifacts");
+    std::fs::create_dir_all(&artifacts_path)?;
 
     let mut cmd = args::Args::command();
 
     let man = clap_mangen::Man::new(cmd.clone());
-    let mut manpage_file = std::fs::File::create(outdir_path.join("svg2pdf.1"))?;
+    let mut manpage_file = std::fs::File::create(artifacts_path.join("svg2pdf.1"))?;
     man.render(&mut manpage_file)?;
 
     for shell in Shell::value_variants() {
-        generate_to(*shell, &mut cmd, "svg2pdf", outdir_path).unwrap();
+        generate_to(*shell, &mut cmd, "svg2pdf", &artifacts_path).unwrap();
     }
 
     Ok(())
