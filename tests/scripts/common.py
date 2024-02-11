9496051a526753fee2bb7f727598ba149372f07d
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-02-11 23:47:15 +0100

    Add gen-typst script

---
+++ tests/scripts/common.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent.parent
+SVG_DIR = ROOT / "svg"
+REF_DIR = ROOT / "ref"
+
+
+class TestFile:
+    def __init__(self, path: Path):
+        self.svg_path = path
+
+        ref_path = path.with_suffix(".png")
+        self.ref_path = Path(REF_DIR / ref_path.relative_to(SVG_DIR))
+
+    def has_ref(self) -> bool:
+        return self.ref_path.is_file()
--- tests/scripts/gen-tests.py
+++ tests/scripts/gen-tests.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python3
 
+from common import SVG_DIR, ROOT, TestFile
 from pathlib import Path
 
-ROOT = Path(__file__).parent.parent
-SVG_DIR = ROOT / "svg"
-REF_DIR = ROOT / "ref"
-
-print(SVG_DIR.resolve())
-
-
-class TestFile:
-    def __init__(self, path: Path):
-        self.svg_path = path
-
-        ref_path = path.with_suffix(".png")
-        parts = list(ref_path.parts)
-        parts[0] = REF_DIR
-        self.ref_path = Path(*parts)
-
-
-OUT_PATH = (ROOT / "src" / "render.rs")
+OUT_PATH = ROOT / "src" / "render.rs"
 
 
 def main():
-
     test_string = "#![allow(non_snake_case)]\n\n"
     test_string += "use crate::render;\n\n"
 
@@ -31,25 +14,27 @@ def main():
         if p.is_file() and p.suffix == ".svg":
             test_file = TestFile(p)
 
-            function_name = str(test_file.svg_path.relative_to(SVG_DIR).with_suffix("")) \
-                .replace("/", "_") \
-                .replace("-", "_") \
-                .replace("=", "_") \
-                .replace(".", "_") \
+            function_name = (
+                str(test_file.svg_path.relative_to(SVG_DIR).with_suffix(""))
+                .replace("/", "_")
+                .replace("-", "_")
+                .replace("=", "_")
+                .replace(".", "_")
                 .replace("#", "")
+            )
             svg_path = test_file.svg_path
             ref_path = test_file.ref_path
 
-            if not ref_path.is_file():
+            if not test_file.has_ref():
                 test_string += "#[ignore] "
 
             test_string += "#[test] "
 
-            test_string += f"fn {function_name}() {{assert_eq!(render(\"{svg_path}\", \"{ref_path}\"), 0)}}\n"
+            test_string += f'fn {function_name}() {{assert_eq!(render("{svg_path}", "{ref_path}"), 0)}}\n'
 
     with open(Path(OUT_PATH), "w") as file:
         file.write(test_string)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
