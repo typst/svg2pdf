a62a276ca63f4cda271c41cd6a7988ae92a69487
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-02-12 14:34:04 +0100

    Remove gen-ref binary

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,33 +1,28 @@
 #[rustfmt::skip]
-mod render;
+mod test;
 
 use std::cmp::max;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use image::io::Reader;
 use image::{Rgba, RgbaImage};
-use lazy_static::lazy_static;
 use once_cell::sync::Lazy;
 use oxipng::{InFile, OutFile};
 use pdfium_render::pdfium::Pdfium;
 use pdfium_render::prelude::{PdfColor, PdfRenderConfig};
 use usvg::{PostProcessingSteps, Tree, TreeParsing, TreePostProc};
-use walkdir::WalkDir;
 
 use svg2pdf::Options;
 
-pub const SVG_DIR: &str = "svg";
-pub const REF_DIR: &str = "ref";
-pub const DIFF_DIR: &str = "diff";
-pub const PDF_DIR: &str = "pdf";
-
+/// The global fontdb instance.
 static FONTDB: Lazy<std::sync::Mutex<fontdb::Database>> = Lazy::new(|| {
     let mut fontdb = fontdb::Database::new();
     fontdb.load_fonts_dir("fonts");
     std::sync::Mutex::new(fontdb)
 });
 
+/// The global pdfium instance.
 static PDFIUM: Lazy<std::sync::Mutex<Pdfium>> = Lazy::new(|| {
     let pdfium = Pdfium::new(
         Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path(
@@ -38,82 +33,7 @@ static PDFIUM: Lazy<std::sync::Mutex<Pdfium>> = Lazy::new(|| {
     std::sync::Mutex::new(pdfium)
 });
 
-lazy_static! {
-    pub static ref SVG_FILES: Vec<PathBuf> = {
-        WalkDir::new(SVG_DIR)
-            .sort_by_file_name()
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|e| e.file_type().is_file())
-            .filter(|e| {
-                e.file_name().to_str().map(|s| s.ends_with(".svg")).unwrap_or(false)
-            })
-            .map(|e| e.into_path())
-            .collect()
-    };
-    pub static ref REF_FILES: Vec<PathBuf> = {
-        WalkDir::new(REF_DIR)
-            .sort_by_file_name()
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|e| e.file_type().is_file())
-            .filter(|e| {
-                e.file_name().to_str().map(|s| s.ends_with(".png")).unwrap_or(false)
-            })
-            .map(|e| e.into_path())
-            .collect()
-    };
-}
-
-#[derive(Eq, PartialEq)]
-pub struct TestFile {
-    raw_path: PathBuf,
-}
-
-/// An abstract representation of a test file that allows to get the paths of the same file
-/// in its different formats (e.g. the original svg file, the pdf file, the reference image).
-impl TestFile {
-    pub fn new(path: &Path) -> Self {
-        let mut stripped_path = path.with_extension("svg");
-
-        if stripped_path.starts_with(SVG_DIR) {
-            stripped_path = PathBuf::from(stripped_path.strip_prefix(SVG_DIR).unwrap());
-        } else if stripped_path.starts_with(REF_DIR) {
-            stripped_path = PathBuf::from(stripped_path.strip_prefix(REF_DIR).unwrap());
-        }
-
-        TestFile { raw_path: stripped_path }
-    }
-
-    fn convert_path(&self, prefix: &Path, extension: &str) -> PathBuf {
-        let mut path_buf = PathBuf::new();
-        path_buf.push(prefix);
-        path_buf.push(self.raw_path.as_path());
-        path_buf = path_buf.with_extension(extension);
-        path_buf
-    }
-
-    pub fn as_raw_path(&self) -> PathBuf {
-        self.raw_path.clone()
-    }
-
-    pub fn as_svg_path(&self) -> PathBuf {
-        self.convert_path(Path::new(SVG_DIR), "svg")
-    }
-
-    pub fn as_ref_path(&self) -> PathBuf {
-        self.convert_path(Path::new(REF_DIR), "png")
-    }
-
-    pub fn as_diff_path(&self) -> PathBuf {
-        self.convert_path(Path::new(DIFF_DIR), "png")
-    }
-
-    pub fn as_pdf_path(&self) -> PathBuf {
-        self.convert_path(Path::new(PDF_DIR), "pdf")
-    }
-}
-
+/// Converts a PDF into a png image.
 pub fn render_pdf(pdf: &[u8]) -> RgbaImage {
     let pdfium = PDFIUM.lock().unwrap();
     let document = pdfium.load_pdf_from_byte_slice(pdf, None);
@@ -134,6 +54,7 @@ pub fn render_pdf(pdf: &[u8]) -> RgbaImage {
     result
 }
 
+/// Converts an SVG string into a usvg Tree
 pub fn read_svg(svg_string: &str) -> Tree {
     let options = usvg::Options::default();
     let mut tree = Tree::from_str(svg_string, &options).unwrap();
@@ -142,6 +63,8 @@ pub fn read_svg(svg_string: &str) -> Tree {
     tree
 }
 
+/// Converts an image into a PDF and returns the PDF as well as a rendered version
+/// of it.
 pub fn convert_svg(svg_string: &str) -> (Vec<u8>, RgbaImage) {
     let scale_factor = 1.0;
     let tree = read_svg(svg_string);
@@ -157,6 +80,7 @@ pub fn convert_svg(svg_string: &str) -> (Vec<u8>, RgbaImage) {
     (pdf, image)
 }
 
+/// Saves an RGBA image to a path.
 pub fn save_image(image: &RgbaImage, path: &Path) {
     image.save_with_format(path, image::ImageFormat::Png).unwrap();
 
@@ -168,6 +92,7 @@ pub fn save_image(image: &RgbaImage, path: &Path) {
     .unwrap();
 }
 
+/// Checks if two pixels are different.
 fn is_pix_diff(pixel1: &Rgba<u8>, pixel2: &Rgba<u8>) -> bool {
     if pixel1.0[3] == 0 && pixel2.0[3] == 0 {
         return false;
@@ -179,7 +104,8 @@ fn is_pix_diff(pixel1: &Rgba<u8>, pixel2: &Rgba<u8>) -> bool {
         || pixel1.0[3] != pixel2.0[3]
 }
 
-pub fn render(svg_path: &str, ref_path: &str, diff_path: &str, replace: bool) -> i32 {
+/// Runs a single test instance.
+pub fn run_test(svg_path: &str, ref_path: &str, diff_path: &str, replace: bool) -> i32 {
     let expected_image = Reader::open(ref_path).unwrap().decode().unwrap().into_rgba8();
 
     let (_, actual_image) = convert_svg(&fs::read_to_string(svg_path).unwrap());
